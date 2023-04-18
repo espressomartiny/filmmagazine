bb955d5a1564b09aa298009e3aee6dab5d905a31
Martina Vokáčová <martinavokacova@gmail.com>
2023-04-19 00:13:16 +0200

    styly

--- src/components/Movies/index.jsx
+++ src/components/Movies/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import MovieList from "../MovieList";
 import { Outlet } from "react-router-dom";
-import "./style.css"
+import "./style.css";
 
 const Movies = () => {
   return (
     <div className="movies">
       <h1>Movies</h1>
       <div className="movies-container">
-        <MovieList/>
-        <Outlet/>
-        </div>
+        <MovieList />
+        <Outlet />
+      </div>
     </div>
   );
 };
